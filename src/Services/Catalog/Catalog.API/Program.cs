20c16958635f7e36e7100ccb4c92fa05b46a3a00
Alex Santos <alxmath@gmail.com>
2024-07-14 13:08:30 -0300

    Validation Pipeline Behaviour With MediatR using Fluent Validation Library

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,4 @@
+using BuildingBlocks.Behaviors;
 using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
 
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
