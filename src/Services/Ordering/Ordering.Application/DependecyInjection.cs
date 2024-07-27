b819e215db52628c595155727d895c258ac68f10
Alex Santos <alxmath@gmail.com>
2024-07-27 19:58:37 -0300

    Adding Dependency Injections Between Clean Architecture Layers

---
+++ src/Services/Ordering/Ordering.Application/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Application;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        //services.AddMediatR(cfg =>
+        //{
+        //    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        //});
+
+        return services;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infraestructure/DepedencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infraestructure;
+
+public static class DepedencyInjection
+{
+    public static IServiceCollection AddInfraestructureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Database");
+
+        //services.AddDbContext<ApplicationDbContext>(options =>
+        //    options.UseSqlServer(connectionString));
+
+        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
+}
