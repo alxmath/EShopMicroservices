d181aa060af153426c37ea6c7412bcddc08d7bd6
Alex Santos <alxmath@gmail.com>
2024-08-01 20:39:55 -0300

    Seed Product and Orders in SQL Server OrderDb Using EF Core when App Startup

--- src/Services/Ordering/Ordering.Infraestructure/Data/Extensions/DatabaseExtensions.cs
+++ src/Services/Ordering/Ordering.Infraestructure/Data/Extensions/DatabaseExtensions.cs
@@ -19,8 +19,8 @@ public static class DatabaseExtensions
     private static async Task SeedAsync(ApplicationDbContext context)
     {
         await SeedCustomerAsync(context);
-        //await SeedProductAsync(context);
-        //await SeedOrderandItemsAsync(context);
+        await SeedProductAsync(context);
+        await SeedOrdersWithItemsAsync(context);
     }
 
     private static async Task SeedCustomerAsync(ApplicationDbContext context)
@@ -28,6 +28,24 @@ public static class DatabaseExtensions
         if (!await context.Customers.AnyAsync())
         {
             await context.Customers.AddRangeAsync(InitialData.Customers);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedProductAsync(ApplicationDbContext context)
+    {
+        if (!await context.Products.AnyAsync())
+        {
+            await context.Products.AddRangeAsync(InitialData.Products);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedOrdersWithItemsAsync(ApplicationDbContext context)
+    {
+        if (!await context.Orders.AnyAsync())
+        {
+            await context.Orders.AddRangeAsync(InitialData.OrdersWithItems);
             await context.SaveChangesAsync();
         }
     }
