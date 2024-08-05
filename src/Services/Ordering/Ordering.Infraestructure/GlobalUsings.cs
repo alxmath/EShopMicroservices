a3371951d34987feda8f5fc4341bce879c416557
alxmath <alxmath@gmail.com>
2024-08-05 08:01:29 -0300

    Develop SaveChangesInterceptor to Auditing Entities for Order Entities

--- src/Services/Ordering/Ordering.Infraestructure/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.Infraestructure/GlobalUsings.cs
@@ -1,5 +1,6 @@
-﻿global using Ordering.Infraestructure;
+﻿global using Ordering.Infraestructure.Data;
+global using Ordering.Infraestructure.Data.Interceptors;
 global using Microsoft.EntityFrameworkCore;
-global using Ordering.Infraestructure.Data;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Abstractions;
